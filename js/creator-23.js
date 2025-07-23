a1b68ca1bd824e5d248d8485eb35cf941bf48776
AModSoul <109749060+AModSoul@users.noreply.github.com>
2025-07-23 00:18:13 -0400

    Update creator-23.js

--- js/creator-23.js
+++ js/creator-23.js
@@ -3919,10 +3919,8 @@ function writeText(textObject, targetContext) {
 					
 					// Add symbol to render queue without drawing immediately
 					if (textObject.manaPrefix && 
-						(getManaSymbol(textObject.manaPrefix + possibleCode) != undefined || 
-						getManaSymbol(textObject.manaPrefix + possibleCode.split('').reverse().join('')) != undefined)) {
-						manaSymbol = getManaSymbol(textObject.manaPrefix + possibleCode) || 
-									getManaSymbol(textObject.manaPrefix + possibleCode.split('').reverse().join(''));
+						(getManaSymbol(textObject.manaPrefix + possibleCode) != undefined || getManaSymbol(textObject.manaPrefix + possibleCode.split('').reverse().join('')) != undefined)) {
+						manaSymbol = getManaSymbol(textObject.manaPrefix + possibleCode) || getManaSymbol(textObject.manaPrefix + possibleCode.split('').reverse().join(''));
 					} else {
 						manaSymbol = getManaSymbol(possibleCode) || getManaSymbol(possibleCode.split('').reverse().join(''));
 					}
